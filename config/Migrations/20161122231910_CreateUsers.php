d21d35e331bf5e8c563be92a16c2e154d7ec4e2d
Touhidur Rahman <tanimkg@gmail.com>
2016-11-23 10:37:25 +0100

    Added font awesome lib in prototype

---
+++ config/Migrations/20161122231910_CreateUsers.php
@@ -0,0 +1,81 @@
+<?php
+use Migrations\AbstractMigration;
+
+class CreateUsers extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('users');
+        $table->addColumn('first_name', 'string', [
+            'default' => null,
+            'limit' => 60,
+            'null' => false,
+        ]);
+        $table->addColumn('last_name', 'string', [
+            'default' => null,
+            'limit' => 60,
+            'null' => false,
+        ]);
+        $table->addColumn('username', 'string', [
+            'default' => null,
+            'limit' => 40,
+            'null' => false,
+        ]);
+        $table->addColumn('password', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('gender', 'string', [
+            'default' => null,
+            'limit' => 20,
+            'null' => false,
+        ]);
+        $table->addColumn('address', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('city_id', 'integer', [
+            'default' => null,
+            'limit' => 11,
+            'null' => false,
+        ]);
+        $table->addColumn('status', 'integer', [
+            'default' => null,
+            'limit' => 2,
+            'null' => false,
+        ]);
+        $table->addColumn('contact_number', 'string', [
+            'default' => null,
+            'limit' => 60,
+            'null' => false,
+        ]);
+        $table->addColumn('photo', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('reset_key', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('created', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('modified', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->create();
+    }
+}
