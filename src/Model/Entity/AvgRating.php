f1c6cb2ec4bc825bed00ffd42cf161b9df331000
Touhidur Rahman <touhidrahman1987@gmail.com>
2016-12-12 23:36:55 +0100

    Created AvgRating model, search method updated to allow landlord rating

---
+++ src/Model/Entity/AvgRating.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Model\Entity;
+
+use Cake\ORM\Entity;
+
+/**
+ * AvgRating Entity
+ *
+ * @property int $user_id
+ * @property float $avg_rate
+ *
+ * @property \App\Model\Entity\User $user
+ */
+class AvgRating extends Entity
+{
+
+}
