3f650a9bc4a9fd33ea5f4a4ec1b63e1dc820d5dc
Touhidur Rahman <touhidrahman1987@gmail.com>
2016-12-14 15:58:43 +0100

    Migration: added boosted_till column in properties

--- src/Model/Entity/Property.php
+++ src/Model/Entity/Property.php
@@ -72,9 +72,66 @@ class Property extends Entity
 
     /**
      * Virtual field
+     * Return studierent score calculated using other field values
      * @author Touhidur Rahman
      */
-     protected function _getZipcode() {
-        //  return $this->_properties
+     protected function _getStudierentScore() {
+         $score = 0;
+         // based on room and flat size, give some points (Max 10)
+         $sizeFactor = $this->_properties['total_size'] / $this->_properties['room_size'] * 100;
+         if ($sizeFactor <= 40) {
+             $score += 5;
+         } else if ($sizeFactor <= 60) {
+             $score += 8;
+         } else {
+             $score += 10;
+         }
+         // 10 points for these 3
+         if ($this->_properties['description'])                     $score += 2;
+         if (strlen($this->_properties['description']) > 400)       $score += 2;
+         if ($this->_properties['transportation'])                  $score += 2;
+         if (strlen($this->_properties['transportation']) > 200)    $score += 1;
+         if ($this->_properties['house_rules'])                     $score += 2;
+         if (strlen($this->_properties['house_rules']) > 100)       $score += 1;
+
+         // 5 points for looking_for
+         switch ($this->_properties['looking_for']) {
+             case 'Male':
+                 $score += 3;
+                 break;
+
+             case 'Female':
+                 $score += 3;
+                 break;
+
+             default:
+                 $score += 5;
+                 break;
+         }
+
+         // 10 points for availability
+         $from = $this->_properties['available_from'];
+         $to = $this->_properties['available_until'];
+         $availabilityFactor = $from->diffInDays($to);
+         if ($availabilityFactor <= 60) {
+             $score += 4;
+         } else if ($availabilityFactor <= 180) {
+             $score += 6;
+         } elseif ($availabilityFactor < 365) {
+             $score += 8;
+         } else {
+             $score += 10;
+         }
+
+         // 10 points for rent deposit utility_cost
+         if ($this->_properties['deposit'] < 2 * $this->_properties['rent']) {
+             $score += 8;
+         } else {
+             $score += 4;
+         }
+         if ($this->_properties['utility_cost'] < ($this->_properties['rent'] / 5)) $score += 2;
+
+        // 10 points for distance
+        
      }
 }
